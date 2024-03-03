6bd20a3ddd8b6a1618c59a6f577b35845dbe8fa0
Klavier <klaviernavarro@gmail.com>
2024-03-03 09:07:33 +0100

    [ADDED] Challenge 002 - Getting to know you

---
+++ challenge002.py
@@ -0,0 +1,15 @@
+#Challenge 002 - Getting to know you
+print("What's your name?")
+name = input()
+print("What's you favorite food?")
+food = input()
+print("What's your favorite singer or band?")
+music = input()
+print("Where do you live?")
+place = input()
+
+print(f"""Getting to know you!
+You are {name}
+You are probably hungry for {food}
+You're definitely getting your groove on to {music}
+Living in the amazing {place}""")
