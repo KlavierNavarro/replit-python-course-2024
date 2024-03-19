6e0c083c836c0fe14db5f8fab98ca77375498a7a
Klavier <69750276+KlavierNavarro@users.noreply.github.com>
2024-03-19 16:43:02 +0100

    [ADDED] Challenge 018 - Guess the Number

---
+++ challenge018.py
@@ -0,0 +1,21 @@
+#Challenge 018 - Guess the Number
+import random
+
+print("Welcome to the Guess the Number Game!")
+number = random.randint(1, 1000000)
+guess = 0
+attempts = 1
+
+while guess != number:
+  guess = int(input("Guess a number between 1 and 1000000: "))
+  if guess < 0:
+    print("Hey, that's cheating! Game over!")
+    exit()
+  elif guess < number:
+    print("That's too low, try again!")
+    attempts += 1
+  elif guess > number:
+    print("That's too high, try again!")
+    attempts += 1
+  else:
+    print(f"You won! It took you {attempts} attempt(s)")
